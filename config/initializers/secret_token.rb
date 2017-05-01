16e07bd5c2a218a843497e0b4fe2a8375799d2b5
FTDaniel <daniel.reynolds@fieldturf.com>
2017-05-01 17:23:51 -0400

    trial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'bd2b74c8b50e2e39b7ed063fbfcd770df9b9f9317ad7038f2ad53601886737755e06fe0e0985006a6619eb2b8d6d8f811948b9e0c2b4c8a951fbb449f95bd372'
